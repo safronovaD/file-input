99abe42f70eb4cc103b2e021eb0c59b2ed20a0b5
Daria Safronova <safronova.d101@gmail.com>
2024-03-29 12:29:11 +0300

    настройка проекта

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MainPage from "@/pages/MainPage.vue";
+</script>
+
+<template>
+  <MainPage/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Main</h1>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
