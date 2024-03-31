14da350993cf692e254bab5daa1b5f736c90c36e
Daria Safronova <safronova.d101@gmail.com>
2024-03-31 17:16:45 +0300

    доработки

--- src/components/FileInput/FileInput.vue
+++ src/components/FileInput/FileInput.vue
@@ -5,8 +5,10 @@
       <Button v-if="!fileName" :text="'Выбрать файл'" :disabled="disabled" class="file-input__button" @click="generateClickEvent"/>
       <Button v-else-if="isFileUploading" :text="'Отменить'" class="file-input__button" @click="resetFileInput"/>
       <Button v-else :text="'Удалить'" class="file-input__button" @click="deleteFile"/>
+
       <img v-if="isFileUploading" src="@/assets/images/circular-progress-indicator.svg" class="file-input__loader" alt="Загрузка"/>
       <label for="file" class="file-input__name" @click="onLabelClick">{{ labelText }}</label>
+
       <input id="file" type="file" name="file" class="file-input__input" ref="fileInput" @change="uploadFile">
     </div>
     <div v-if="showError" class="file-input__error">{{ errorMessage }}</div>
@@ -39,7 +41,7 @@ const props = defineProps({
 
 const emits = defineEmits(['fileUploaded', 'fileDeleted']);
 
-const reader = new FileReader();
+const reader: FileReader = new FileReader();
 
 let fileInput = ref(),
     fileName = ref<String>(''),
@@ -51,7 +53,7 @@ const labelText = computed(() => {
 });
 
 function generateClickEvent(): void {
-  const event = new MouseEvent('click');
+  const event: Event = new MouseEvent('click');
   fileInput.value.dispatchEvent(event);
 }
 
@@ -95,58 +97,4 @@ function onLabelClick(event: Event): void {
 }
 </script>
 
-<style scoped>
-.file-input__label {
-  font-weight: 500;
-  font-size: 13px;
-  line-height: 120%;
-  color: var(--color-neutral-900);
-  margin-bottom: 12px;
-}
-
-.file-input__input {
-  position: absolute;
-  top: 0;
-  left: 0;
-  opacity: 0;
-}
-
-.file-input__wrapper {
-  display: flex;
-  align-items: center;
-  margin-bottom: 8px;
-}
-
-.file-input__button {
-  margin-right: 16px;
-}
-
-.file-input__button:focus {
-  outline: none;
-  box-shadow: 0 0 0 2px rgba(107, 114, 128, 0.1);
-}
-
-.file-input__loader {
-  margin-right: 8px;
-  animation: rotation 2s infinite linear;
-}
-
-.file-input__name {
-  font-size: 14px;
-  line-height: 114%;
-  letter-spacing: -0.01em;
-  color: var(--color-neutral-400);
-}
-
-.file-input__error {
-  font-size: 13px;
-  line-height: 120%;
-  color: var(--color-error-500);
-}
-
-.file-input__hint {
-  font-size: 13px;
-  line-height: 120%;
-  color: var(--color-neutral-500);
-}
-</style>
+<style src="./style.css" scoped/>
